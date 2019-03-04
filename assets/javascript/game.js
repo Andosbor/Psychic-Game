3f470e44b48d3c1a23e4944c16d268e6350230cb
Oogapooki <andrewosborn101@gmail.com>
2019-03-03 22:53:39 -0600

    adding more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
 //making an array filled with every letter in the alphabet
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+var guesses = ["9","8","7","6","5","4","3","2","1","0",]
+
 var w;
 
 var l;
+
+var g;
 
 
 //determines what key the computer chooses
@@ -15,21 +19,27 @@ document.onkeyup = function(event){
 //determines which key is being pressed
     var userGuess = event.key;
 
- //if correct guess, add one to win and reset game. (console.log part doesn't currently work)
+ //if correct guess, add one to win and reset game. (need to figure out how to add for every win)
     if (userGuess === computerGuess){
-        document.getElementById("wins").innerHTML = "1";
+        document.getElementById("wins").innerHTML = +1;
         console.log("yes");
     }
 
 //find out how to increase win number on document when user wins each time**
     
-//If wrong guess, lose one try.
+//If wrong guess, lose one try. (need to figure out how to subtract from 9 lives and display it)
 
     if (userGuess !== computerGuess){
+
+        
 //lose 1 life starting with 9
-    
+
             // document.write([l--]);
     }
+    for (g = 0; g < guesses.length; g++){
+
+    }
+
 
       //If lives are <1, add a loss and reset game
 
